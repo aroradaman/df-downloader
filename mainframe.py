9aa0b81c827710e550f0d2871ac83e181def43bf
daman1807 <aroradaman@gmail.com>
2015-08-20 11:51:07 +0530

    post methods

--- mainframe.py
+++ mainframe.py
@@ -11,6 +11,7 @@ import random
 import time
 import psutil
 from Queue import Queue
+import requests
 
 class downloader :
 	def __init__(self) :
@@ -212,14 +213,18 @@ class downloader :
 			else :
 				workSplit.append([start+1,start+frag])
 			start += frag
-		workSplit[-1][1] = size	
-		#print '\n##########    DOWNLOAD DISTRIBUTION   #################\n'
+		workSplit[-1][1] = size		
 		for i in range(len(active_peers)) :
 			self.global_dict[self.md5(filename)]['range'].append([int(workSplit[i][0]),int(workSplit[i][1])])
-			req_url = "http://" + active_peers[i] + ":" + str(self.config['server_port']) + "/local_init?start=" + str(workSplit[i][0]) + "&end=" + str(workSplit[i][1]) + "&url=" + urllib.quote(url) + "&reporting_ip=" + self.config['home_ip'] + "&file_name=" + filename + "&self_ip=" + active_peers[i]
-			urllib2.urlopen(req_url)
-			#print str(i+1) + '.  ' + ' CLIENT - ' +  str(active_peers[i])  + '    START - ' + str(workSplit[i][0]) + '		END - ' + str(workSplit[i][1])
-		#print '\n###################				 ###################\n'
+			req_url = "http://" + active_peers[i] + ":" + str(self.config['server_port']) + "/local_init"			
+			post_data = {
+				'start'	 		: workSplit[i][0],
+				'end' 			: workSplit[i][1],
+				'url'	 		: url,
+				'reporting_ip'	: self.config['home_ip'],
+				'file_name'		: filename
+			}
+			req = requests.post(req_url,data=post_data)			
 
 	def local_init(self,start,end,url,file_name,reporting_ip) :
 		local_id = self.md5(file_name+str(start))
@@ -233,9 +238,9 @@ class downloader :
 				work_split.append([start+1,start+frag])
 			start += frag
 		work_split[-1][1] = end		
-		for i in range(self.config['threads']) :
+		for i in range(self.config['threads']) :			
 			self.local_dict[local_id].update({'threads_completed':0,'url':url,'reporting_ip':reporting_ip,'file':file_name,'home_ip':self.config['home_ip'],str(i):{'start':work_split[i][0],'end':work_split[i][1],'data':[]}})
-			self.pool.add_task(self.basic_downloader,i,work_split[i][0],work_split[i][1],url,reporting_ip,file_name,local_id)
+			self.pool.add_task(self.basic_downloader,i,local_id)
 			#threading.Thread(target=self.basic_downloader,args=(i,work_split[i][0],work_split[i][1],url,reporting_ip,file_name,local_id)).start()		
 		self.job_configs.update({ self.md5(file_name) : 
 														{   
@@ -247,10 +252,10 @@ class downloader :
 														} 
 													})
 		
-	def basic_downloader(self,thread_num,START,END,url,IP,file_name,local_id) :
-		content = ''
-		req = urllib2.Request(url)
-		req.headers["Range"]='bytes='+str(START)+'-'+str(END)
+	def basic_downloader(self,thread_num,local_id) :
+		content = ''		
+		req = urllib2.Request(self.local_dict[local_id]['url'])
+		req.headers["Range"]='bytes='+str(self.local_dict[local_id][str(thread_num)]['start'])+'-'+str(self.local_dict[local_id][str(thread_num)]['end'])
 		f = urllib2.urlopen(req)
 		while True :
 			data = f.read()
@@ -258,43 +263,39 @@ class downloader :
 				break
 			content += data
 		self.local_dict[local_id]['threads_completed'] += 1
-		self.local_dict[local_id][str(thread_num)]['data'] = [START,content]
-		#print str(self.local_dict[local_id]['threads_completed']*100/self.config['threads']) + '% done for ' + file_name + '  for job ' + local_id
+		self.local_dict[local_id][str(thread_num)]['data'] = [self.local_dict[local_id][str(thread_num)]['start'],content]		
 		if self.local_dict[local_id]['threads_completed'] == self.config['threads'] :
-			threading.Thread(target=self.local_data_assembly,args=(file_name,self.local_dict[local_id]['reporting_ip'],local_id)).start()
+			threading.Thread(target=self.local_data_assembly,args=(local_id,)).start()
 	
-	def local_data_assembly(self,file_name,reporting_ip,local_id) :
-		#print "\nEnterered local assembler \n"
+	def local_data_assembly(self,local_id) :
+		file_name = self.local_dict[local_id]['file']
+		reporting_ip = self.local_dict[local_id]['reporting_ip']
 		content = ''.join( item[1] for item in sorted([ self.local_dict[local_id][str(i)]['data'] for i in range(self.config['threads']) ],key=itemgetter(0)))
 		with open(local_id,'wb') as f :
 			f.write(content)
-		#print '\nCompleted For' , local_id , '\n'
-		start = self.local_dict[local_id]['0']['start']
-		url = "http://" + reporting_ip + ":" + str(self.config['server_port']) + "/fetch_local_data?local_id=" + local_id + "&ip=" + self.config['home_ip'] + "&start=" + str(start) + "&file_name=" + file_name
-		req = urllib2.urlopen(url)		
+		req_url = "http://" + self.local_dict[local_id]['reporting_ip'] + ":" + str(self.config['server_port']) + "/fetch_local_data"
+		post_data = {
+			'start'	 		: self.local_dict[local_id]['0']['start'],			
+			'file_name'		: self.local_dict[local_id]['file']
+		}
+		req = requests.post(req_url,data=post_data)		
 
 	def fetch_local_data(self,local_id,ip,start,file_name) :
-		url = 'http://' + ip + ':' + str(self.config['server_port']) + '/local_transfer?local_id=' + local_id
-		f = urllib2.urlopen(url)
-		content = ''
-		while True :
-			data = f.read()
-			if not data :
-				break
-			content += data
-		#print '\nFetching url ' , url , '\n'		
+		req_url = 'http://' + ip + ':' + str(self.config['server_port']) + '/local_transfer'
+		post_data = {
+			'local_id' : local_id			
+		}
+		req = requests.post(req_url,data=post_data)
+		content = req.text
 		self.global_dict[self.md5(file_name)]['data'].append([int(start),content])
 		if len(self.global_dict[self.md5(file_name)]['data']) == len(self.global_dict[self.md5(file_name)]['active_peers']):
 			threading.Thread(target=self.global_data_assembley,args=(file_name,)).start()
 
-	def global_data_assembley(self,file_name) :
-		#print "\nEnterered global assembler \n"
+	def global_data_assembley(self,file_name) :		
 		content = ''.join( item[1] for item in sorted(self.global_dict[self.md5(file_name)]['data'],key=itemgetter(0)))
 		with open(file_name,'wb') as f :
 			f.write(content)
-		#print "\nAssembled Successfully " + file_name + "\n"
-		#self.global_dict.pop(self.md5(file_name),None)
-
+		
 class Worker(threading.Thread) :
 	def __init__(self, tasks) :
 		threading.Thread.__init__(self)
